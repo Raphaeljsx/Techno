3a3cb5015e8940faec78776799a3ead9f836a15d
Raphaeljsx <raphaelalvoliv@gmail.com>
2023-06-27 23:55:11 -0300

    Add files

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false,
+    carrinho:[],
+  },
+  filters: {
+    numeroPreco(valor){
+      return valor.toLocaleString("pt-BR",{style: "currency", currency: "BRL" }) //Converte a moeda
+    }
+  },
+  computed:{
+    carrinhoTotal(){
+      let total = 0
+      if(this.carrinho.length){    
+        this.carrinho.forEach(item => { return total+= item.preco})
+        }
+      return total;
+    }
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("../../api/produtos.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+          
+        });
+    },
+    abrirModal(id){
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fetchProduto(id){
+      fetch(`../../api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        })
+    },
+    fecharModal({target, currentTarget}){
+      if(target === currentTarget) this.produto = false;
+    },
+    adicionarItem(){
+      this.produto.estoque--;
+      const {id, nome, preco} = this.produto
+      console.log(id, nome, preco);
+      this.carrinho.push({id, nome, preco});
+    },
+    removerItem(index){
+      this.carrinho.splice(index, 1);
+    },
+    checarLocalStorage(){
+      if(window.localStorage.carrinho){
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+      }
+    }
+  },
+  watch:{
+    carrinho(){
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    }
+  },
+  created() {
+    this.fetchProdutos();
+    this.checarLocalStorage();
+  },
+});
